442f9b33eb406169cd64b238e9f40789ddb94c7f
abibib <130450204+abibib@users.noreply.github.com>
2023-05-23 09:43:27 +0200

    yes

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 let background = [
   "fond vert.png",
-  "fond rose.png",
-  "fond bleu.png"
+  "fond rose (1).png",
+  "fond bleu (1).png"
 ]
 
 function Brice() {
@@ -28,4 +28,56 @@ document.querySelector(".boutonfeuilles").addEventListener("click", function () 
   }
 
   document.querySelector(".premiereplante").src = images[indiceImage];
+
 });
+
+const imagefenetres = ["fenetre 2.png", "fenetre.png", "fenetre4.png"];
+
+document.querySelector(".boutonfenêtre").addEventListener("click", function () {
+  indiceImage++;
+
+  if (indiceImage >= imagefenetres.length) {
+    indiceImage = 0;
+  }
+  document.querySelector(".fenetre1").src = imagefenetres[indiceImage];
+  document.querySelector(".fenetre2").src = imagefenetres[indiceImage];
+  document.querySelector(".fenetre3").src = imagefenetres[indiceImage];
+});
+
+
+var monboutonjour = document.querySelector(".monboutonjour");
+var monboutonnuit = document.querySelector(".monboutonnuit");
+var imageAffichee = document.getElementById("imageAffichee");
+
+monboutonjour.addEventListener("click", function () {
+  imageAffichee.src = "jour.png";
+});
+
+monboutonnuit.addEventListener("click", function () {
+  imageAffichee.src = "nuit.png";
+
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var rendutexte = document.getElementById("rendutexte");
+  var textarea = document.getElementById("monTextarea");
+  
+  textarea.addEventListener("input", function(){
+    var textTape = textarea.value;
+    rendutexte.innerHTML = textTape;
+  })
+
+
+})
+
+var monTextarea = document.getElementById("monTextarea");
+var valeurTextarea = monTextarea.value;
+
+var boutonNuit = document.getElementById("monboutonnuit");
+var contenu = document.getElementById("contenu");
+
+boutonNuit.addEventListener("click", function() {
+  contenu.classList.toggle("filternuit");
+});
+
+
